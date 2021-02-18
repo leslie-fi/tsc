7ffd9e344babb347de8f5d9d89a4db2550710216
leslie-fi <leslie.turis1@gmail.com>
2021-02-18 12:30:56 -0500

    finished sorter

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,24 +1,16 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    // collection: number[];
-    function Sorter(collection) {
-        this.collection = collection;
-        // this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
+const numCollection = new NumbersCollection_1.NumbersCollection([-10, 3, -5, 0]);
+const charCollection = new CharactersCollection_1.CharactersCollection('eyQuvAd1w');
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+linkedList.sort();
+linkedList.print();
+charCollection.sort();
+console.log(charCollection);
