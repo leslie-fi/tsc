48ba492a866197fcf949e04df4bd6281c7eb4689
leslie-fi <leslie.turis1@gmail.com>
2021-02-18 15:51:23 -0500

    csv refactor 1 abstractclass +generic

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -5,31 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
-const utils_1 = require("./utils");
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
         this.data = [];
     }
     read() {
-        this.data = fs_1.default.readFileSync(this.filename, {
+        this.data = fs_1.default
+            .readFileSync(this.filename, {
             encoding: 'utf-8',
         })
             .split('\n')
             .map((row) => {
             return row.split(',');
         })
-            .map((row) => {
-            return [
-                utils_1.dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                +row[3],
-                +row[4],
-                row[5],
-                row[6]
-            ];
-        });
+            .map(this.mapRow);
     }
 }
 exports.CsvFileReader = CsvFileReader;
--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,31 +1,20 @@
 import fs from 'fs';
-import {MatchResult} from './MatchResult'
-import { dateStringToDate } from './utils';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
-
+  
+  abstract mapRow(row: string[]): T;
   read(): void {
-    this.data = fs.readFileSync(this.filename, {
-      encoding: 'utf-8',
-    })
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          +row[3],
-          +row[4],
-          row[5] as MatchResult,
-          row[6]
-        ]
-      })
+      .map(this.mapRow);
   }
 }
