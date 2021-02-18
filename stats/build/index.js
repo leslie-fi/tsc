99574fd817db5833536632701256d8b9c11c9587
leslie-fi <leslie.turis1@gmail.com>
2021-02-18 15:29:57 -0500

    csv before refactor 1

---
+++ stats/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+console.log(reader.data[0][0]);
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+    else if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+}
+console.log(manUnitedWins);
---
+++ stats/src/index.ts
@@ -0,0 +1,19 @@
+import {CsvFileReader} from './CsvFileReader';
+import {MatchResult} from './MatchResult';
+const reader = new CsvFileReader('football.csv')
+reader.read()
+console.log(reader.data[0][0])
+
+
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  } else if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
