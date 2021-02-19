7b43a9189ada96d2ac4bba54038c3d7b8970b467
leslie-fi <leslie.turis1@gmail.com>
2021-02-19 17:39:03 -0500

    mega User class b4 refactor 1

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hola mundo')
+import { User } from './models/User';
+import axios from 'axios';
+
+const user1 = new User({ id: 2 });
+user1.set({name: 'lisa marie joyce', age: 22})
+// user1.save()
+const newbUser = new User({ name: 'Wino4ever', age: 500 });
+
+// newbUser.save();
+user1.fetch()
