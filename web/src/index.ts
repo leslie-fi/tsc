1c48b2134ccdb12c10abebd95496bd809c4c8939
leslie-fi <leslie.turis1@gmail.com>
2021-02-22 15:08:56 -0500

    fix strict nullcheck error on rootelement

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,12 @@
 import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
-let rootElement = document.getElementById('root');
-const user = User.buildUser({name: 'NaTe', age: 31});
-const userForm = new UserForm(rootElement, user);
+const rootElement = document.getElementById('root');
+const user = User.buildUser({ name: 'NaTe', age: 31 });
 
-userForm.render();
+if (rootElement) {
+  const userForm = new UserForm(rootElement, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
