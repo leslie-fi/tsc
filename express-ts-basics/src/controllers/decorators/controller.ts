5b4ddd3b66fe6306e74d6338b2725fe1fec6f424
leslie-fi <leslie.turis1@gmail.com>
2021-02-26 16:53:52 -0500

    use decorator/middleware

--- express-ts-basics/src/controllers/decorators/controller.ts
+++ express-ts-basics/src/controllers/decorators/controller.ts
@@ -10,11 +10,22 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
 
-      const path = Reflect.getMetadata(MetadataKeys.Path, target.prototype, key);
-      const method: Methods = Reflect.getMetadata(MetadataKeys.Method, target.prototype, key);
+      const path = Reflect.getMetadata(
+        MetadataKeys.Path,
+        target.prototype,
+        key
+      );
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.Method,
+        target.prototype,
+        key
+      );
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.Middleware, target, key) || [];
+
       if (path) {
-        console.log(method,path );
-        router[method](`${routePrefix}${path}`, routeHandler);
+        console.log(method, path);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
