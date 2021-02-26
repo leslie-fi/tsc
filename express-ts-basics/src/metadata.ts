de89fe6e0b4122cdd52ad8e51e66e7a846fbf8da
leslie-fi <leslie.turis1@gmail.com>
2021-02-26 14:36:57 -0500

    direction for refactor

--- express-ts-basics/src/metadata.ts
+++ express-ts-basics/src/metadata.ts
@@ -1,26 +1,25 @@
 import 'reflect-metadata';
-
-@printMetadata
+import { router } from './routes/loginRoutes';
+ 
 class Plane {
   color: string = 'red';
-
-  @markFunction('hidden secret shh')
+ 
   fly(): void {
     console.log('vrrrrr')
   }
 }
 
-// factory decorator
-function markFunction(secretInfo: string){
-return function (target: Plane, key: string) {
-  Reflect.defineMetadata('secret', secretInfo, target, key)
-}
+function get(path:string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key)
+  }
 }
 
-
-function printMetadata(target: typeof Plane) {
-  for (let key in target.prototype) {
-    const secret = Reflect.getMetadata('secret', target.prototype, key)
-    console.log(secret)
+function controller(target:typeof Plane) {
+  for (var key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key)
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key)
+    
+    router.get(path, target.prototype[key])
   }
 }
